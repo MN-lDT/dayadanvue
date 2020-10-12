a5ab9ac11124465a07808aa1335661ef7ce238ee
dtli <dtli@iflytek.com>
2020-10-12 18:23:36 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
 
+
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -69,6 +70,7 @@ module.exports = {
       }
     ]
   },
+
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
